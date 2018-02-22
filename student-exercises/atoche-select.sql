7dad1141c8ad70b3dc94f0fc708a212b1c4e1f35
CATOCHE <mrcatoche@gmail.com>
2018-02-22 09:43:40 -0500

    Translating requirements to SQL

--- student-exercises/atoche-select.sql
+++ student-exercises/atoche-select.sql
@@ -5,4 +5,5 @@ SELECT * FROM individual WHERE LastName = 'Jennings';
 SELECT ID, LASTNAME FROM individual WHERE DeceasedDate IS NOT NULL;
 SELECT ID, LastName, BirthDate FROM individual WHERE Year(BirthDate) > 1940;
 SELECT * FROM individual WHERE ID IN (1,3,5,7,19);
-
+SELECT * FROM band WHERE genre = 'Alternative' AND isTogether <> 1;
+SELECT ID, name FROM band WHERE ID = 4;
